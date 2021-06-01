2240739a7ec10dfd1faa11e10ddef2952d2f226f
Collin Lanier <collinlanier12@gmail.com>
2021-06-01 18:25:27 -0400

    added antd layout, populated table with spring data

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,19 +1,103 @@
-import  {Button} from "antd";
-
+import {useState, useEffect} from "react";
 import API from "./lib/API";
+import { Layout, Menu, Breadcrumb, Table } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
 import './App.css';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    },
+];
 
 const App = () => {
 
-    API.Students.getAll()
-        .then(response => response.data)
-        .then(console.log)
-        .catch(err => console.log(err.message));
+    const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+
+    const getStudents = () =>{
+        API.Students.getAll()
+            .then(response => setStudents(response.data))
+            .catch(err => console.log(err.message));
+    }
+
+    useEffect(() => {
+        getStudents();
+    },[]);
+
+    const renderStudents = () => {
+        if(students.length <= 0){
+            return "no data available";
+        }
+        return <Table dataSource={students} columns={columns} />;
+    }
 
     return (
-      <div className="App">
-        <Button type="primary">Hello</Button>
-      </div>
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed}
+                   onCollapse={setCollapsed}>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                    <Menu.Item key="1" icon={<PieChartOutlined />}>
+                        Option 1
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<DesktopOutlined />}>
+                        Option 2
+                    </Menu.Item>
+                    <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                        <Menu.Item key="3">Tom</Menu.Item>
+                        <Menu.Item key="4">Bill</Menu.Item>
+                        <Menu.Item key="5">Alex</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                        <Menu.Item key="6">Team 1</Menu.Item>
+                        <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="9" icon={<FileOutlined />}>
+                        Files
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className="site-layout">
+                <Header className="site-layout-background" style={{ padding: 0 }} />
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                        {renderStudents()}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
     );
 }
 
