adee292e18e6a36a2d5bcd6944168073a1a485b2
Collin Lanier <collinlanier12@gmail.com>
2021-05-31 20:24:16 -0400

    initlize of spring project

---
+++ src/main/java/com/example/fullstackspring/FullstackspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fullstackspring/FullstackspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
